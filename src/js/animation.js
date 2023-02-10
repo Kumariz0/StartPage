fbf3b44323c9f5f691ce51334c72b4f5f1eaed7c
Heck <jodokus.heck@komatsu-mining.de>
2023-02-10 14:26:24 +0100

    added the link changing and replaced the SVGs

--- src/js/animation.js
+++ src/js/animation.js
@@ -42,3 +42,66 @@ function playAnimation() {
             "-=1800"
         );
 }
+
+function changesvg(elementIn, elementOut) {
+    if (elementIn == ".work-svg") {
+        anime({
+            targets: elementIn,
+            translateX: ["0px", "58px"],
+            easing: 'easeInOutExpo',
+        });
+        anime({
+            targets: elementOut,
+            translateY: ["58px", "0px"],
+            easing: 'easeInOutExpo',
+        });
+    }
+    if (elementIn == ".home-svg") {
+        anime({
+            targets: elementIn,
+            translateY: ["0px", "58px"],
+            easing: 'easeInOutExpo',
+        });
+        anime({
+            targets: elementOut,
+            translateX: ["58px", "0px"],
+            easing: 'easeInOutExpo',
+        });
+    }
+}
+
+function startAnimation(elementIn) {
+    if (elementIn == ".work-svg") {
+        anime({
+            targets: elementIn,
+            translateX: ["0px", "58px"],
+            easing: 'easeInOutExpo',
+        });
+    } else {
+        anime({
+            targets: elementIn,
+            translateY: ["0px", "58px"],
+            easing: 'easeInOutExpo',
+        });
+    }
+}
+
+function removelinkanimation() {
+    anime({
+        targets: "main ul",
+        opacity: {
+            value: 0,
+            duration: 10,
+            easing: 'linear'
+        },
+    });
+
+}
+function restorelinkanimation() {
+    anime({
+        targets: "main ul, h3",
+        opacity: [0, 1],
+        translateY: [10, 0],
+        delay: anime.stagger(20),
+    });
+}
--- src/js/cookies.js
+++ src/js/cookies.js
@@ -1,10 +1,11 @@
 // cookies 🍪
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 //set a cookie
 function setCookie(name, value, days) {
     try {
         document.cookie = name + "=" + value + "; SameSite=None; Secure" + "; expires=Thu, 13 Jan 2000 12:00:00 UTC" + "; path=/";
     } catch (error) {
-        alert("error")
+        alert("error");
     }
     document.cookie = name + "=" + value + "; SameSite=None; Secure" + "; expires=Thu, 13 Jan 2100 12:00:00 UTC" + "; path=/";
 }
@@ -12,16 +13,14 @@ function setCookie(name, value, days) {
 // Get a cookie
 function getCookie(name) {
     var nameEQ = name + "=";
-    var cookies = document.cookie.split(';');
+    var cookies = document.cookie.split(";");
     for (var i = 0; i < cookies.length; i++) {
         var tempcookie = cookies[i];
-        while (tempcookie.charAt(0) == ' ') tempcookie = tempcookie.substring(1, tempcookie.length);
+        while (tempcookie.charAt(0) == " ") tempcookie = tempcookie.substring(1, tempcookie.length);
         if (tempcookie.indexOf(nameEQ) == 0) return tempcookie.substring(nameEQ.length, tempcookie.length);
     }
     return null;
 }
-
-
 
 // Save the toggle button state to a cookie
 function saveToggleButtonState() {
@@ -29,23 +28,133 @@ function saveToggleButtonState() {
         setCookie("toggledButton", 0);
     } else {
         if (getCookie("toggledButton") == 0) {
-            setCookie("toggledButton", 1)
-            document.getElementById("mode-svg").src = "images/work.svg"
-
+            setCookie("toggledButton", 1);
+            changesvg(".work-svg", ".home-svg");
+            removelinkanimation();
+            changeLinks(1);
+            restorelinkanimation();
         } else {
-            setCookie("toggledButton", 0)
-            document.getElementById("mode-svg").src = "images/home.svg"
+            changesvg(".home-svg", ".work-svg");
+            setCookie("toggledButton", 0);
+            removelinkanimation();
+            changeLinks(0);
+            restorelinkanimation();
         }
     }
 }
 
-
-
 // Restore the toggle button state from a cookie
 function restorePageState() {
     if (getCookie("toggledButton") == 0) {
-        document.getElementById("mode-svg").src = "images/home.svg"
+        startAnimation(".home-svg");
     } else {
-        document.getElementById("mode-svg").src = "images/work.svg"
+        startAnimation(".work-svg");
+    }
+    changeLinks(getCookie("toggledButton"));
+}
+
+async function changeLinks(pagestate) {
+    //if pagestate is "home"
+    if (pagestate == 0) {
+        var link_1_1 = "https://youtube.com";
+        var link_1_2 = "https://twitch.com";
+        var link_1_3 = "https://twitter.com";
+        var link_2_1 = "https://netflix.com";
+        var link_2_2 = "https://9anime.to/home";
+        var link_2_3 = "https://www.amazon.de/Amazon-Video/b?ie=UTF8&node=3010075031";
+        var link_3_1 = "https://www.seaofthieves.com/insider";
+        var link_3_2 = "https://www.merfolkslullaby.com/de";
+        var link_3_3 = "https://seaofthieves.fandom.com/wiki/Sea_of_Thieves_Wiki";
+        var link_4_1 = "https://grabfreegames.com/";
+        var link_4_2 = "https://leafguides.xyz/";
+        var link_4_3 = "https://open.spotify.com";
+
+        var _1_1 = document.getElementById("1.1");
+        var _1_2 = document.getElementById("1.2");
+        var _1_3 = document.getElementById("1.3");
+        var _2_1 = document.getElementById("2.1");
+        var _2_2 = document.getElementById("2.2");
+        var _2_3 = document.getElementById("2.3");
+        var _3_1 = document.getElementById("3.1");
+        var _3_2 = document.getElementById("3.2");
+        var _3_3 = document.getElementById("3.3");
+        var _4_1 = document.getElementById("4.1");
+        var _4_2 = document.getElementById("4.2");
+        var _4_3 = document.getElementById("4.3");
+        _1_1.innerHTML = "Youtube";
+        _1_1.setAttribute("href", link_1_1);
+        _1_2.innerHTML = "Twitch";
+        _1_2.setAttribute("href", link_1_2);
+        _1_3.innerHTML = "Twitter";
+        _1_3.setAttribute("href", link_1_3);
+        _2_1.innerHTML = "Netlifx";
+        _2_1.setAttribute("href", link_2_1);
+        _2_2.innerHTML = "9Anime";
+        _2_2.setAttribute("href", link_2_2);
+        _2_3.innerHTML = "Prime Video";
+        _2_3.setAttribute("href", link_2_3);
+        _3_1.innerHTML = "Insider build";
+        _3_1.setAttribute("href", link_3_1);
+        _3_2.innerHTML = "merfolkslullaby";
+        _3_2.setAttribute("href", link_3_2);
+        _3_3.innerHTML = "SoT fandom";
+        _3_3.setAttribute("href", link_3_3);
+        _4_1.innerHTML = "Free games";
+        _4_1.setAttribute("href", link_4_1);
+        _4_2.innerHTML = "Leaf guides";
+        _4_2.setAttribute("href", link_4_2);
+        _4_3.innerHTML = "Spotify";
+        _4_3.setAttribute("href", link_4_3);
+    } else {
+        //if pagestate is "work"
+        var link_1_1 = "https://example.com";
+        var link_1_2 = "https://example.com";
+        var link_1_3 = "https://example.com";
+        var link_2_1 = "https://example.com";
+        var link_2_2 = "https://example.com";
+        var link_2_3 = "https://example.com";
+        var link_3_1 = "https://example.com";
+        var link_3_2 = "https://example.com";
+        var link_3_3 = "https://example.com";
+        var link_4_1 = "https://example.com";
+        var link_4_2 = "https://example.com";
+        var link_4_3 = "https://example.com";
+
+        var _1_1 = document.getElementById("1.1");
+        var _1_2 = document.getElementById("1.2");
+        var _1_3 = document.getElementById("1.3");
+        var _2_1 = document.getElementById("2.1");
+        var _2_2 = document.getElementById("2.2");
+        var _2_3 = document.getElementById("2.3");
+        var _3_1 = document.getElementById("3.1");
+        var _3_2 = document.getElementById("3.2");
+        var _3_3 = document.getElementById("3.3");
+        var _4_1 = document.getElementById("4.1");
+        var _4_2 = document.getElementById("4.2");
+        var _4_3 = document.getElementById("4.3");
+        _1_1.innerHTML = "example";
+        _1_1.setAttribute("href", link_1_1);
+        _1_2.innerHTML = "example";
+        _1_2.setAttribute("href", link_1_2);
+        _1_3.innerHTML = "example";
+        _1_3.setAttribute("href", link_1_3);
+        _2_1.innerHTML = "example";
+        _2_1.setAttribute("href", link_2_1);
+        _2_2.innerHTML = "example";
+        _2_2.setAttribute("href", link_2_2);
+        _2_3.innerHTML = "example";
+        _2_3.setAttribute("href", link_2_3);
+        _3_1.innerHTML = "example";
+        _3_1.setAttribute("href", link_3_1);
+        _3_2.innerHTML = "example";
+        _3_2.setAttribute("href", link_3_2);
+        _3_3.innerHTML = "example";
+        _3_3.setAttribute("href", link_3_3);
+        _4_1.innerHTML = "example";
+        _4_1.setAttribute("href", link_4_1);
+        _4_2.innerHTML = "example";
+        _4_2.setAttribute("href", link_4_2);
+        _4_3.innerHTML = "example";
+        _4_3.setAttribute("href", link_4_3);
     }
 }
